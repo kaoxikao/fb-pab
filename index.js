ea921e85dcaa476ce7160dc811d10b0995cfbcc4
cuongcao <caovietcuong@gmail.com>
2017-07-29 23:22:31 +0200

    change route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.listen(8181, function() {
     console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     var jsonResponse = [];
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
