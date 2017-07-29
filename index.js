95fb1ddb89a1fa617125d84245ccdf8376c02410
cuongcao <caovietcuong@gmail.com>
2017-07-29 23:33:40 +0200

    change port so it works with heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.listen(8181, function() {
-    console.log('Chatfuel Bot-Server listening on port 80...');
+app.listen(process.env.PORT || 5000, function() {
+    console.log('Chatfuel Bot-Server listening...');
 });
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     var jsonResponse = [];
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
